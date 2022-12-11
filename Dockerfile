f5f8307207f445092f9cf8c771d4a313bc91c440
mubashirzebi <mubashshirzebi@gmail.com>
2022-12-11 10:48:17 +0530

    updated docker file

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM node:16-alpine
 
-WORKDIR /home/scorer-v2
+# WORKDIR /home/scorer-v2
 
 COPY package.json ./
 
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
     watch: './app.ts',
     instances: '-1',
     cron_restart: '0 0 * * *',
+    Interpreter: '/node_modules/pm2/node_modules/.bin/ts-node'
    
   }],
 
