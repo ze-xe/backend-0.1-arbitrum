2feaae81802a82456682b39e01b52ede0aa4a29a
prasad-kumkar <pk111_@live.com>
2022-12-19 14:30:51 +0530

    new deployment

--- src/helper/constant.ts
+++ src/helper/constant.ts
@@ -10,12 +10,12 @@ const Decimals = {
 };
 
 
-const ExchangeAddress = "0x46C9Ad9bA79783017790fc4Bed4364aBB98c22Dd";
-const BtcAddress = "0xf79Af8d59C208e526cF33E4b37b8Eca379Aced0F";
+const ExchangeAddress = "0x9117596d6203a71802803a18a9588C0Af67E762d";
+const BtcAddress = "0xCd037E4E4B71db4d6F0BD3E8Fa524bf4A0f02b91";
 // const BtcAddress = "0xD9458532E2c4e9646ec68040F0Cb6bdD7B911038"; // ethereum
-const UsdcAddress = "0x6405a05e1f83B1849b0F8B720ac844C1dDd59863";
-
-
+const UsdcAddress = "0xC49aFbB504d5B743622eAb23a58E6769328E3Fd8";
+export const EthAddress = "0x90C44082fD902609B44070C8613EF191E93a7811";
+export const ZexeAddress = "0x19b7Ee51d69AA53134e43D966a03EfBB924aCFC4"
 
 
 
